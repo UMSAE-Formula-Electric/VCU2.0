4dabe8e20b99c89d15880c313cf9161ca5b13642
moram03 <mamora0312@gmail.com>
2024-06-08 19:48:55 -0500

    Got Bluetooth Sending Again

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -101,13 +101,13 @@ int main(void)
   MX_USART2_Init();
   MX_TIM12_Init();
   MX_ADC1_Init();
-  MX_USART3_UART_Init();
+  MX_USART3_Init();
   /* USER CODE BEGIN 2 */
     dash_set_all_leds();
 
     HAL_GPIO_WritePin(BT_P2_0_GPIO_Port, BT_P2_0_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(BT_P2_4_GPIO_Port, BT_P2_4_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(BT_EAN_GPIO_Port, BT_EAN_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(BT_EAN_GPIO_Port, BT_EAN_Pin, GPIO_PIN_SET);
     HAL_Delay(500);
 
     HAL_GPIO_WritePin(BT_SW_BTN_GPIO_Port, BT_SW_BTN_Pin, GPIO_PIN_SET);
@@ -116,8 +116,10 @@ int main(void)
     //HAL_GPIO_WritePin(BT_EAN_GPIO_Port, BT_EAN_Pin, GPIO_PIN_SET);
 //    HAL_GPIO_WritePin(BT_P2_0_GPIO_Port, BT_P2_0_Pin, GPIO_PIN_SET);
 //    HAL_GPIO_WritePin(BT_P2_4_GPIO_Port, BT_P2_4_Pin, GPIO_PIN_SET);
-//    HAL_Delay(500);
+    HAL_Delay(5000);
 //    HAL_GPIO_WritePin(BT_EAN_GPIO_Port, BT_EAN_Pin, GPIO_PIN_RESET);
+//    HAL_GPIO_WritePin(BT_SW_BTN_GPIO_Port, BT_SW_BTN_Pin, GPIO_PIN_RESET);
+//    HAL_GPIO_WritePin(BT_RESET_GPIO_Port, BT_RESET_Pin, GPIO_PIN_RESET);
 
 
     if(!logInitialize()){
